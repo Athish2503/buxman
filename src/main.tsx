b4244b34b90b5a844159d31eb3ae06e941e9e91f
Krishneshvar <krishjn07@gmail.com>
2026-05-02 23:02:57 +0530

    refactor: cleanup branch and prepare for migration

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import App from './App.tsx'
-import './index.css'
-
-// Initialize Capacitor PWA elements
-defineCustomElements(window);
-
-createRoot(document.getElementById("root")!).render(<App />);
